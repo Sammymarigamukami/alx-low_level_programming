8daf1f55ddbe750c9bb0de3326a083953610e6a2
sammymariga <marigasam18@gmail.com>
2023-03-16 16:35:41 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - A program that prints the size of various computer types
- * Return 0 (success)
+ * main - A program that prints the size of various types of computers
+ * Return: 0 (success)
  */
 int main(void)
 {
