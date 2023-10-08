38ac18f3851aa63236adcf4ba6cad91d1ec35c88
root <marigasam18@gmail.com>
2023-10-08 05:54:16 -0700

    update

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,21 +1,20 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * get_bit - prog returns the value of a bit at a given index.
- * @n: number to check bits in
- * @index: index at which to check bit
+ * get_bit - returns the value of a bit at an index in a decimal number
+ * @n: number to search
+ * @index: index of the bit
  *
- * Return: value of the bit, or -1 if there is an error
+ * Return: value of the bit
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int divisor, check;
+	int bit_val;
 
-	if (index > (sizeof(unsigned long int) * 8 - 1))
+	if (index > 63)
 		return (-1);
-	divisor = 1 << index;
-	check = n & divisor;
-	if (check == divisor)
-		return (1);
-	return (0);
+
+	bit_val = (n >> index) & 1;
+
+	return (bit_val);
 }
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,15 +1,17 @@
 #include "main.h"
-#include <stdlib.h>
+
 /**
- * clear_bit - This prog sets the value of a bit to 0 at a given index
- * @n: parameter
- * @index: index
- * Return: 1 if success, -1 if error
+ * clear_bit - sets the value of a given bit to 0
+ * @n: pointer to the number to change
+ * @index: index of the bit to clear
+ *
+ * Return: 1 for success, -1 for failure
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > sizeof(n) * 8)
+	if (index > 63)
 		return (-1);
-	*n &= ~(1 << index);
+
+	*n = (~(1UL << index) & *n);
 	return (1);
 }
